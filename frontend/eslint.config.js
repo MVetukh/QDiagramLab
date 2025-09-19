1359aeb7a81c161c871d445847f3a5b5a398e94e
MaxV <m.vetux@yandex.ru>
2025-09-19 03:57:25 +0300

    final fix lint check

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,22 +1,38 @@
-// frontend/eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
     {
-        files: ['**/*.js'],
+        files: ['**/*.js', '**/*.jsx'],
         languageOptions: {
             ecmaVersion: 2021,
             sourceType: 'module',
             globals: {
                 ...globals.browser,
             },
+            parser: '@babel/eslint-parser',
+            parserOptions: {
+                requireConfigFile: false,
+                babelOptions: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        },
+        plugins: {
+            react: require('eslint-plugin-react')
         },
         rules: {
             ...js.configs.recommended.rules,
             'indent': ['error', 4],
             'quotes': ['error', 'single'],
             'semi': ['error', 'always'],
+            'react/jsx-uses-react': 'error',
+            'react/jsx-uses-vars': 'error'
         },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        }
     },
 ];
